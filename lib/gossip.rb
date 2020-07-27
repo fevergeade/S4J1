7157b639c042520189ce3b60796686788039db08
vergeade <fevergeade@gmail.com>
2020-07-28 00:24:09 +0200

    fin 2.7

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'pry'
 
 class Gossip
   attr_accessor :author, :content
@@ -25,16 +26,22 @@ def self.all
   end
   return all_gossips
 end
-
+#Methode retourner le neme - 1 ^^
 def self.find(id)
   all_gossips = Gossip.all
-  #all_gossips.index { |index| return all_gossips[index] if  index == id}
   return all_gossips[id.to_i-1]
 end
-
+#Methode de update : je n'ai pas trouvé simple update. donc je fais un sav dans un tableau, sup et on recrée
 def self.update (id, nom, contenu)
   all_gossips = Gossip.all
-  
+  all_gossips[id.to_i-1].author = nom
+  all_gossips[id.to_i-1].content = contenu
+  File.delete("./db/gossip.csv")
+  CSV.open("./db/gossip.csv", "ab") do |csv|
+    all_gossips.each do |items|
+      csv << [items.author, items.content]
+    end
+  end
 end
 
 end
